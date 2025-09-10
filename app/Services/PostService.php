d8e0e77387a07d8812172601fa7557f008c446aa
aboodmagdy1 <c.aboodmagdy@gmail.com>
2025-09-10 09:01:38 +0300

    feat: add PostStatusNotification for post acceptance and rejection notifications

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -8,6 +8,7 @@ use Filament\Notifications\Actions\Action;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use App\Notifications\PostStatusNotification;
 
 class PostService extends BaseService
 {
@@ -61,20 +62,28 @@ class PostService extends BaseService
 
     public function accept(Post $post): bool
     {
-        return $post->update([
-            'status' => PostStatus::Approved->value,
-            'accepted_at' => now(),
-            'rejection_reason' => null,
-        ]);
+        $post = tap($post,function($post){
+            $post->update([
+                'status' => PostStatus::Approved->value,
+                'accepted_at' => now(),
+                'rejection_reason' => null,
+            ]);
+        });
+        $this->afterUpdate($post,1);
+        return true ;
     }
 
     public function reject(Post $post, ?string $reason = null): bool
     {
-        return $post->update([
+        $post = tap($post,function($post) use ($reason){
+            $post->update([
             'status' => PostStatus::Rejected->value,
             'rejection_reason' => $reason,
             'accepted_at' => null,
         ]);
+        });
+        $this->afterUpdate($post,2);
+        return true;
     }
 
     protected function afterCreate(Post $post): void
@@ -84,8 +93,54 @@ class PostService extends BaseService
                 ->url(route('filament.admin.resources.posts.view', $post->id))
                 ->label(__('Let\'s review it'))
         ]);
+    }   
+
+    protected function afterUpdate(Post $post ,int $type): void
+    {
+        //type 1 mean accept 
+        //type 2 mean reject
+        $recipent = $post->author;
+
+        $rejection_title = [
+            'en'=>'Your post has been rejected',
+            'ar'=>'المنشور الخاص بك مرفوض',
+        ];
+        $rejection_body = [
+            'en'=>'Your post has been rejected',
+            'ar'=>'المنشور الخاص بك مرفوض',
+        ];
+
+        $acceptance_title = [   
+            'en'=>'Your post has been accepted',
+            'ar'=>'المنشور الخاص بك مقبول',
+        ];
+        $acceptance_body = [
+            'en'=>'Your post has been accepted',
+            'ar'=>'المنشور الخاص بك مقبول',
+        ];
+        $data = [
+            'title'=>[
+                'en'=>$type === 1 ? $acceptance_title['en'] : $rejection_title['en'],
+                'ar'=>$type === 1 ? $acceptance_title['ar'] : $rejection_title['ar'],
+            ],
+            'body'=>[
+                'en'=>$type === 1 ? $acceptance_body['en'] : $rejection_body['en'],
+                'ar'=>$type === 1 ? $acceptance_body['ar'] : $rejection_body['ar'],
+            ],
+            'metadata'=>[
+                'type'=>$type === 1 ? 'accepted' : 'rejected',
+                'route'=>'posts.show',
+                'post_id'=>$post->id,
+            ]
+        ];
+        $recipent->notify(new PostStatusNotification($post,$data));
+        $recipent->customNotifications()->create([
+            'title' => $data['title'],
+            'body' => $data['body'],
+            'metadata' => $data['metadata'],
+        ]);
     }
-    
+
     public function listFeed($per_page=15)
     {
         return $this->post->newQuery()
