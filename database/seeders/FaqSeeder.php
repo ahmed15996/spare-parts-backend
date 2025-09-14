197e5527a0ba0a8b1ecc2875afd326a9fd7d3b53
aboodmagdy1 <c.aboodmagdy@gmail.com>
2025-09-14 15:13:38 +0300

    feat: add initial FAQ entries with multilingual support for contact information

--- database/seeders/FaqSeeder.php
+++ database/seeders/FaqSeeder.php
@@ -12,6 +12,27 @@ class FaqSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $questions = [
+            [
+                'title' => [
+                    'en' => 'What is the best way to contact you?',
+                    'ar' => 'كيف يمكنني التواصل معك؟',
+                ],
+                'description' => [
+                    'en' => 'You can contact us via email or phone.',
+                    'ar' => 'يمكنك التواصل معنا عبر البريد الإلكتروني أو الهاتف.',
+                ],
+            ],
+            [
+                'title' => [
+                    'en' => 'What is the best way to contact you?',
+                    'ar' => 'كيف يمكنني التواصل معك؟',
+                ],
+                'description' => [
+                    'en' => 'You can contact us via email or phone.',
+                    'ar' => 'يمكنك التواصل معنا عبر البريد الإلكتروني أو الهاتف.',
+                ],
+            ],
+        ];
     }
 }
