ea7a12ac5e89ef3baa836d427ca9aae55d73e2b7
aboodmagdy1 <c.aboodmagdy@gmail.com>
2025-09-19 16:53:43 +0300

    fix: update avatar URL logic in CommentResource to use role name for accurate media retrieval

--- app/Http/Resources/API/V1/CommentResource.php
+++ app/Http/Resources/API/V1/CommentResource.php
@@ -11,6 +11,7 @@ class CommentResource extends JsonResource
     {
         $user = $request->user();
         $isAuthor = $this->author_id === $user->getKey() && $this->author_type === get_class($user);
+        $role = $this->author->roles->last();
         return [
             'id' => $this->id,
             'content' => $this->content,
@@ -18,7 +19,7 @@ class CommentResource extends JsonResource
             'author' => [
                 'id' => $this->author->id,
                 'name' => $this->author->name,
-                'avatar' => $this->author->role == 'client' ? $this->author->getFirstMediaUrl('avatar') : $this->author->getFirstMediaUrl('logo'),
+                'avatar' => $role->name == 'client' ? $this->author->getFirstMediaUrl('avatar') : $this->author->getFirstMediaUrl('logo'),
             ],
             'created_at' => Carbon::parse($this->created_at)->diffForHumans(),
         ];
