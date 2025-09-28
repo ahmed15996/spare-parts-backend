c16b40631b08afa251970881e748adb8e5d7fa4f
aboodmagdy1 <c.aboodmagdy@gmail.com>
2025-09-28 16:54:49 +0300

    fix: update avatar field in PersonalProfileResource to reference provider's logo for non-client roles

--- app/Http/Resources/API/V1/PersonalProfileResource.php
+++ app/Http/Resources/API/V1/PersonalProfileResource.php
@@ -20,7 +20,7 @@ class PersonalProfileResource extends JsonResource
             'last_name' => $this->last_name,
             'email' => $this->email,
             'phone' => $this->phone,
-            'avatar' =>  $this->hasRole('client') ? $this->getFirstMediaUrl('avatar') : $this->getFirstMediaUrl('logo'),
+            'avatar' =>  $this->hasRole('client') ? $this->getFirstMediaUrl('avatar') : $this->provider->getFirstMediaUrl('logo'),
             'address' => $this->address,
             'lat' => $this->lat,
             'long' => $this->long,
